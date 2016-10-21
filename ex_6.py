743dc8a60a37c255f0a0f4411493dd0205d0103d
Sanyok004 <Sanyok004@yandex.ru>
2016-10-21 18:38:18 +0300

    Update ex_6.py

--- ex_6.py
+++ ex_6.py
@@ -1,44 +1,25 @@
-#!/usr/bin/env python3
-import json
-import sys
-from librip.ctxmngrs import timer
-from librip.decorators import print_result
-from librip.gens import field, gen_random
-from librip.iterators import Unique as unique
-
-path = None
-
-# Здесь необходимо в переменную path получить
-# путь до файла, который был передан при запуске
-
-with open(path) as f:
-    data = json.load(f)
-
-
-# Далее необходимо реализовать все функции по заданию, заменив `raise NotImplemented`
-# Важно!
-# Функции с 1 по 3 дожны быть реализованы в одну строку
 # В реализации функции 4 может быть до 3 строк
 # При этом строки должны быть не длиннее 80 символов
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+    return sorted(unique(field(arg, 'job-name'), ignore_case=True), key=str.lower)
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    return list(filter(lambda x: x.startswith('Программист'), arg))
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+    return list(map(lambda x: x + ' с опытом Python', arg))
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
+    salary = list(gen_random(100000, 200000, len(arg)))
+    return list('{}, зарплата {} руб'.format(x, y) for x, y in zip(arg, salary))
 
 
 with timer():
